64ec7962cc352e50ea705533d4c3e3504f969153
Ratna Keerthana <lidiyapinapati@gmail.com>
2026-07-06 22:26:20 +0530

    Fix API base URL

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: https://mern-bookstore-2-gt1e.onrender.com/
+  baseURL: "https://mern-bookstore-2-gt1e.onrender.com/",
 });
 
 export default API;
