03bab49cc90cc98d992d229f48cf2e45b3f5ea5c
Ratna Keerthana <lidiyapinapati@gmail.com>
2026-07-06 22:14:48 +0530

    Update backend URL

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://YOUR-BACKEND.onrender.com/api",
+  baseURL: https://mern-bookstore-2-gt1e.onrender.com/
 });
 
 export default API;
